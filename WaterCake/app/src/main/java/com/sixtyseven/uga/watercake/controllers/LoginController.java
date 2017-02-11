f4374f5984266c1cee48a6c3b91ef958eea45549
Dimitar Pankov <mitelinko@gmail.com>
2017-02-11 01:10:18 -0500

    Addeed registration screen and transitions.

--- WaterCake/app/src/main/java/com/sixtyseven/uga/watercake/controllers/LoginController.java
+++ WaterCake/app/src/main/java/com/sixtyseven/uga/watercake/controllers/LoginController.java
@@ -1,6 +1,7 @@
 package com.sixtyseven.uga.watercake.controllers;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -21,6 +22,11 @@ public class LoginController extends Activity {
 
     public void attemptLogin(View view) {
         Log.d("login", "login attempted." + " username: " + ((EditText) findViewById(R.id.usernameTextbox)).getText() + " password: " + ((EditText) findViewById(R.id.passwordTextbox)).getText());
+    }
+
+    public void goToRegistration(View view) {
+        Log.d("here", "bug");
+        startActivity(new Intent(LoginController.this, RegistrationController.class));
     }
 
 }
---
+++ WaterCake/app/src/main/java/com/sixtyseven/uga/watercake/controllers/RegistrationController.java
@@ -0,0 +1,20 @@
+package com.sixtyseven.uga.watercake.controllers;
+
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.sixtyseven.uga.watercake.R;
+
+/**
+ * Created by Dimitar on 2/11/2017.
+ */
+
+public class RegistrationController extends Activity {
+
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.register);
+    }
+    
+}
