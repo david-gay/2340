a8320a120ffc07cbb56ab4c0793c4ae16a221ca0
Dimitar Pankov <mitelinko@gmail.com>
2017-02-11 00:33:33 -0500

    Created project and implmented login screen.
    Created the project for the app. Implmented a basic dummy login page,
    that logs the username and password that were entered.

---
+++ WaterCake/app/src/main/java/com/sixtyseven/uga/watercake/controllers/LoginController.java
@@ -0,0 +1,26 @@
+package com.sixtyseven.uga.watercake.controllers;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+
+import com.sixtyseven.uga.watercake.R;
+
+/**
+ * Created by Dimitar on 2/10/2017.
+ */
+
+public class LoginController extends Activity {
+
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+    }
+
+    public void attemptLogin(View view) {
+        Log.d("login", "login attempted." + " username: " + ((EditText) findViewById(R.id.usernameTextbox)).getText() + " password: " + ((EditText) findViewById(R.id.passwordTextbox)).getText());
+    }
+
+}
