899f29cc6df667e3753e37de876459a116a2c2f6
Blake Harper <blakerobertharper@gmail.com>
2017-03-04 14:09:41 -0500

    Report manager now uses Location for reports

--- WaterCake/app/src/main/java/com/sixtyseven/uga/watercake/models/report/ReportManager.java
+++ WaterCake/app/src/main/java/com/sixtyseven/uga/watercake/models/report/ReportManager.java
@@ -1,5 +1,6 @@
 package com.sixtyseven.uga.watercake.models.report;
 
+import com.sixtyseven.uga.watercake.models.pins.Location;
 import com.sixtyseven.uga.watercake.models.report.constants.WaterCondition;
 import com.sixtyseven.uga.watercake.models.report.constants.WaterPurityCondition;
 import com.sixtyseven.uga.watercake.models.report.constants.WaterType;
@@ -41,11 +42,11 @@ public class ReportManager {
      * @param condition the condition of that water
      * @return true if the report is created and added
      */
-    public boolean createWaterReport(String authorUsername, WaterType waterType, WaterCondition
+    public boolean createWaterReport(String authorUsername, Location location, WaterType waterType, WaterCondition
             condition) {
         //if validation passes {
         waterSourceReports.put(nextReportId, new WaterSourceReportImpl(nextReportId, authorUsername, new Date(),
-                waterType, condition));
+                location, waterType, condition));
 
         nextReportId++;
 
@@ -60,12 +61,12 @@ public class ReportManager {
      * @param condition the condition of that water
      * @return true if the report is created and added
      */
-    public boolean createPurityReport(String authorUsername, WaterType waterType, WaterCondition
+    public boolean createPurityReport(String authorUsername, Location location, WaterType waterType, WaterCondition
             condition, WaterPurityCondition waterPurityCondition, float virusPPM, float
                                               contaminantPPM) {
 
         WaterSourceReportImpl potentialReport = new WaterSourceReportImpl(nextReportId, authorUsername, new Date(),
-                waterType, condition, waterPurityCondition, virusPPM, contaminantPPM);
+                location, waterType, condition, waterPurityCondition, virusPPM, contaminantPPM);
 
 
 
